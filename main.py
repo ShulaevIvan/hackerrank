31d9503b44a13026576b36f7e009b69edd4a4690
Shulaev Ivan <mail@ntdservice.ru>
2022-12-02 00:14:03 +0300

    initial

---
+++ main.py
@@ -0,0 +1,57 @@
+
+# def get_numbers(num):
+#     counter = 0
+#     while (counter < num):
+#         if (num > 0):
+#             print(counter * counter)
+#             counter += 1
+
+# if __name__ == '__main__':
+#     get_numbers(int(input()))
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = map(int, input().split())
+
+#     print(sorted(set(arr), reverse=True)[1])
+
+
+# 5
+# Harry
+# 37.21
+# Berry
+# 37.21
+# Tina
+# 37.2
+# Akriti
+# 41
+# Harsh
+# 39
+
+# grades = [
+#     (input(), float(input()))
+#     for _ in range(int(input()))
+# ]
+
+# lowest = min(score for _, score in grades)
+# grades = [(name, score) for name, score in grades if score > lowest]
+
+# lowest = min(score for _, score in grades)
+# grades = [(name, score) for name, score in grades if score == lowest]
+
+# grades.sort()
+# for name, _ in grades:
+#     print(name)
+
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = []
+#     for i in range(n):
+#         i += 1
+#         arr.append(str(i))
+    
+#     print(''.join(arr))
+
+    
+
